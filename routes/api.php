f0e27664c8df5134df1b2302c8e0cb79d07855d4
Kanushka Gayan <kanushkanet@gmail.com>
2021-10-30 10:45:31 +0530

    add entites

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\DeliveryController;
+use App\Http\Controllers\MaterialController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +27,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::apiResource('accounts', AccountController::class);
+Route::apiResource('deliveries', DeliveryController::class);
+Route::apiResource('materials', MaterialController::class);
+Route::apiResource('orders', OrderController::class);
+Route::apiResource('permissions', PermissionController::class);
+Route::apiResource('suppliers', SupplierController::class);
+Route::apiResource('transactions', TransactionController::class);
+Route::apiResource('users', UserController::class);
