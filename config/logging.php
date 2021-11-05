9d30d571f151b1c19275adeb44869dcdfd3ce07a
Kanushka Gayan <kanushkanet@gmail.com>
2021-11-05 16:06:40 +0530

    use errorlog

--- config/logging.php
+++ config/logging.php
@@ -50,7 +50,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['errorlog'],
             'ignore_exceptions' => false,
         ],
 
